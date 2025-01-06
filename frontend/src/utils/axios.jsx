7f3f4290a708c07ef54bd3ea07f601cf21b001c5
yashdangar <yashdangar123@gmail.com>
2025-01-06 14:19:38 +0530

    Update Backend URL

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "http://ec2-13-61-15-101.eu-north-1.compute.amazonaws.com:3000/",
     headers: {
         accept: 'application/json'
     }
